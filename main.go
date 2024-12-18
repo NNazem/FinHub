bc10d0e6a68694815e2f64e0be0c52ef019b2d1e
U0N7778 <navid.nazem@esternibisp.com>
2024-12-18 11:20:57 +0100

    Revert "First implementation of the apis to get account balance"
    This reverts commit 5a495d42aab185cdd705d754de7a368957ffa5b9.

--- main.go
+++ main.go
@@ -12,26 +12,11 @@ func main() {
 		return
 	}
 
-	//banks, err := fetchAllBanksByCountry(token, "IT")
+	banks, err := fetchAllBanksByCountry(token, "IT")
 
-	bank, err := fetchBankById(token, "INTESA_SANPAOLO_BCITITMMXXX")
-
-	agreement, err := CreateUserAgreement(bank.Id, token)
-
-	link, err := CreateLink(bank.Id, agreement.Id, token)
-
-	accounts, err := FetchUserAccountsByBank(link.ID, token)
-
-	balance, err := FetchAccontBalance(agreement.Id, token)
 	log.Println(token.AccessToken)
 
-	log.Println(bank.Name)
-
-	log.Println(agreement.Id)
-
-	log.Println(link.Link)
-
-	log.Println(accounts.Accounts)
-
-	log.Println(balance.Balances[0].BalanceAmount.Amount)
+	for i := range banks {
+		log.Println(banks[i])
+	}
 }
