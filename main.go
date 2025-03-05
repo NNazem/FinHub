f499b129f5ee1763cfaf6a7d5c429b4759ee87b9
navidnazem <nazem.navid@gmail.com>
2025-03-05 20:52:45 +0100

    Added docker-compose.yml

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ func main() {
 
 	go func() {
 		for {
-			log.Println("Updating crypto data!")
+			log.Println("Updating crypto data")
 			time.Sleep(10 * time.Hour)
 			err := CoinmarketcapService.GetCoinsHistoricalData()
 			if err != nil {
